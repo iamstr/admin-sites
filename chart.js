9f8ff5ca0fb652a8b718a198c695fca33c718c3d
iamstr <asatar1994@gmail.com>
2020-12-27 04:47:46 +0300

    added widgets

---
+++ chart.js
@@ -0,0 +1,22 @@
+var ctx = document.getElementById("myChart").getContext("2d");
+var chart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: "bar",
+
+  // The data for our dataset
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgb(75, 225, 105)",
+        borderColor: "rgb(75, 225, 105)",
+        data: [0, 10, 5, 2, 20, 30, 45]
+      }
+    ]
+  },
+
+  // Configuration options go here
+  options: {}
+});
+export default chart;
